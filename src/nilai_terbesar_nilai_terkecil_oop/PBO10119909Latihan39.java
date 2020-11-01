abd03ea8aa4cbef03dfce8d57c0f5a3e901cd6af
anggawanridho <anggawanridho@gmail.com>
2020-11-01 15:35:03 +0700

    first commit

---
+++ src/nilai_terbesar_nilai_terkecil_oop/PBO10119909Latihan39.java
@@ -0,0 +1,31 @@
+package nilai_terbesar_nilai_terkecil_oop;
+
+import java.util.Scanner;
+
+public class PBO10119909Latihan39 {
+
+    public static void main(String[] args) {
+        Penentu mahasiswa = new Penentu();
+        Scanner scanner = new Scanner(System.in);
+        int i, jumlahMahasiswa, nilaiTerbesar = 0, nilaiTerkecil;
+        String petugas;
+
+        System.out.println("=====Program Nilai Terbesar dan Terkecil Nilai Mahasiswa======");
+        petugas = mahasiswa.namaPetugas();
+        jumlahMahasiswa = mahasiswa.getBanyak_mahasiswa();
+        int[] nilai = new  int[jumlahMahasiswa];
+        for (i =0; i < jumlahMahasiswa; i++) {
+            System.out.printf("Nilai Mahasiswa ke-%d\t\t\t\t= ",i+1);
+            nilai[i] = scanner.nextInt();
+        }
+        System.out.println();
+        System.out.println("======Hasil Nilai Mahasiswa======");
+        nilaiTerbesar = mahasiswa.nilaiTerbesar(nilai);
+        nilaiTerkecil = mahasiswa.nilaiTerkecil(nilai);
+        System.out.println();
+        System.out.println("Nilai Terbesar Adalah "+nilaiTerbesar);
+        System.out.println("Nilai Terkecil Adalah "+nilaiTerkecil);
+        System.out.println();
+        System.out.print("Petugas : "+petugas);
+    }
+}
---
+++ src/nilai_terbesar_nilai_terkecil_oop/Penentu.java
@@ -0,0 +1,46 @@
+package nilai_terbesar_nilai_terkecil_oop;
+
+import java.util.Scanner;
+
+/**
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class Penentu {
+    Scanner scanner = new Scanner(System.in);
+    private int i, banyak_mahasiswa, terbesar, terkecil;
+    String petugas;
+
+
+    public String namaPetugas() {
+        System.out.print("Masukkan Nama Petugas\t\t\t\t= ");
+        return petugas = scanner.next();
+    }
+
+    public int getBanyak_mahasiswa() {
+        System.out.print("Masukkan Banyaknya Nilai Mahasiswa\t= ");
+        return banyak_mahasiswa = scanner.nextInt();
+    }
+
+    public int nilaiTerkecil(int [] nilai) {
+        terkecil = nilai[0];
+        for (i = 0; i < banyak_mahasiswa; i++) {
+            if (nilai[i] < terkecil) {
+                terkecil = nilai[i];
+            }
+        }
+        return terkecil;
+    }
+
+    public int nilaiTerbesar(int [] nilai) {
+        for (i = 0; i < banyak_mahasiswa; i++) {
+            if (nilai[i] > terbesar) {
+                terbesar = nilai[i];
+            }
+        }
+        return terbesar;
+    }
+
+}
